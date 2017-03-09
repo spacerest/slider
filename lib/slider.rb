5da8404c0c5a4fa0554bb7fafac875bc66f89279
spacerest <robconvo@gmail.com>
2017-03-08 19:04:17 -0600

    first test

--- lib/slider.rb
+++ lib/slider.rb
@@ -2,4 +2,13 @@ require 'board'
 
 class Slider
 
+def initialize
+
 end
+
+def play_game
+end
+
+
+end
+
--- spec/slider_spec.rb
+++ spec/slider_spec.rb
@@ -1,10 +1,17 @@
 require 'spec_helper'
 require 'slider'
+require 'board'
 #when spec helper changes paths like currently, 'require_relative 'mazer'' raises error
 
-describe Slider do
+describe Board do
+  let(:board) { Board.new }
+  context "can make a two by one slider" do
+    
+    it "takes height 1 and width 2 and creates the right array" do
+      board.make(1,2).eq [[1,nil]] 
+    end 
 
-  context "can make a two by two slider" do
+  
   end
 
 end 
