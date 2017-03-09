66b4de11c680cd968cc9db6b48bc0985d09f9e89
Cody Fuller <git@codyfuller.com>
2017-03-08 20:19:29 -0600

    We can move left

--- lib/board.rb
+++ lib/board.rb
@@ -5,22 +5,31 @@ class Board
   def initialize(height, width)
     @height = height
     @width = width
-    # @matrix = [[1,nil]]
     populate_board
   end
 
   def populate_board
     self.matrix = (1..(height * width)).to_a.each_slice(width).to_a
-    # width.times do
   end
 
   def move(direction)
+    emp = find_empty_space
     if direction == "L"
-    self.matrix.map! { |i|
-      i.reverse
-   }
+      temp = matrix[emp[0]][emp[1]-1]
+      self.matrix[emp[0]][emp[1]-1] = matrix[emp[0]][emp[1]]
+      self.matrix[emp[0]][emp[1]] = temp
     elsif direction == "R"
       raise RuntimeError
+    end
+  end
+
+  def find_empty_space
+    matrix.each_with_index do |h, h_idx|
+      h.each_with_index do |w, w_idx|
+        if w == height * width
+          return [h_idx, w_idx]
+        end
+      end
     end
   end
 
--- spec/slider_spec.rb
+++ spec/slider_spec.rb
@@ -32,6 +32,18 @@ describe Board do
       expect{board.move("R")}.to raise_error(RuntimeError)
     end
 
+    it "left in a 3 X 3" do
+      board = Board.new(3, 3)
+      board.move('L')
+      expect(board.matrix).to eq [[1, 2, 3], [4, 5, 6], [7, 9, 8]]
+    end 
   end 
 
+  context 'board' do
+    it 'find the empty space' do
+      board = Board.new(2, 2)
+      board.matrix = [[2, 4], [1, 3]]
+      expect(board.find_empty_space).to eq [0, 1]
+    end
+  end
 end 
